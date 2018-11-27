b60d62d8273e5095a285197a1276369060653ee1
flafflewaffle <sssingh575@gmail.com>
2018-11-27 16:46:58 +0000

    Location Layer Plugin swapped for Location Component, followed Mapbox Documentation

--- app/src/main/java/com/example/s1623165/coinz/Map.kt
+++ app/src/main/java/com/example/s1623165/coinz/Map.kt
@@ -21,6 +21,7 @@ import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
 import com.mapbox.mapboxsdk.location.modes.CameraMode
 import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
@@ -30,18 +31,14 @@ import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
 
 import kotlinx.android.synthetic.main.activity_map.*
 
-class Map : AppCompatActivity(), OnMapReadyCallback,
-        LocationEngineListener, PermissionsListener {
+class Map : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
 
     private val tag = "MapActivity"
     private var mapView: MapView? = null
     private var map: MapboxMap? = null
+    private var locationComponent: LocationComponent? = null
 
-    private lateinit var originLocation : Location
     private lateinit var permissionsManager : PermissionsManager
-    private lateinit var locationEngine : LocationEngine
-    private lateinit var locationLayerPlugin : LocationLayerPlugin
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -72,11 +69,24 @@ class Map : AppCompatActivity(), OnMapReadyCallback,
         }
     }
 
+    private fun enableLocationComponent() {
+        locationComponent = map?.locationComponent
+        locationComponent?.activateLocationComponent(this)
+        locationComponent?.isLocationComponentEnabled = true
+
+        // Set location engine interval times
+        locationComponent?.locationEngine?.interval = 5000
+        locationComponent?.locationEngine?.fastestInterval = 1000
+
+        // Set the component's camera mode
+        locationComponent?.cameraMode = CameraMode.TRACKING
+        locationComponent?.renderMode = RenderMode.NORMAL
+    }
+
     private fun enableLocation() {
         if(PermissionsManager.areLocationPermissionsGranted(this)) {
             Log.d(tag, "Permissions are granted")
-            initialiseLocationEngine()
-            initialiseLocationLayer()
+            enableLocationComponent()
         } else {
             Log.d(tag, "Permissions are not granted")
             permissionsManager = PermissionsManager(this)
@@ -84,57 +94,7 @@ class Map : AppCompatActivity(), OnMapReadyCallback,
         }
     }
 
-    @SuppressWarnings("MissingPermission")
-    private fun initialiseLocationEngine() {
-        locationEngine = LocationEngineProvider(this)
-                .obtainBestLocationEngineAvailable()
-        locationEngine.apply {
-            interval = 5000 // preferably every 5 seconds
-            fastestInterval = 1000 // at most every second
-            priority = LocationEnginePriority.HIGH_ACCURACY
-                    activate()
-        }
-        val lastLocation = locationEngine.lastLocation
-        if (lastLocation != null) {
-            originLocation = lastLocation
-            setCameraPosition(lastLocation)
-        } else { locationEngine.addLocationEngineListener(this) }
-    }
-
-    @SuppressWarnings("MissingPermission")
-    private fun initialiseLocationLayer() {
-        if (mapView == null) { Log.d(tag, "mapView is null") }
-        else {
-            if (map == null) { Log.d(tag, "map is null") }
-            else {
-                locationLayerPlugin = LocationLayerPlugin(mapView!!,
-                        map!!, locationEngine)
-                locationLayerPlugin.apply {
-                    setLocationLayerEnabled(true)
-                    cameraMode = CameraMode.TRACKING
-                    renderMode = RenderMode.NORMAL
-                }
-            }
-        }
-    }
-
-    override fun onLocationChanged(location: Location?) {
-        if (location == null) {
-            Log.d(tag, "[onLocationChanged] location is null")
-        } else {
-            originLocation = location
-            setCameraPosition(originLocation)
-        }
-    }
-
-    @SuppressWarnings("MissingPermission")
-    override fun onConnected() {
-        Log.d(tag, "[onConnected] requesting location updates")
-        locationEngine.requestLocationUpdates()
-    }
-
-    override fun onExplanationNeeded(permissionsToExplain:
-                                     MutableList<String>?) {
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
         Log.d(tag, "Permissions: $permissionsToExplain")
         // Present popup message or dialogue
         val userDialogue = Snackbar.make(
@@ -144,9 +104,8 @@ class Map : AppCompatActivity(), OnMapReadyCallback,
         userDialogue.show()
     }
 
-    private fun setCameraPosition(location: Location) {
-        val latlng = LatLng(location.latitude, location.longitude)
-        map?.animateCamera(CameraUpdateFactory.newLatLng(latlng))
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onPermissionResult(granted: Boolean) {
